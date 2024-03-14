c88bd0fd591bd1674f167ae5e28b4b5667380adf
Yato <117742512+Yatokun26@users.noreply.github.com>
2024-03-14 07:51:59 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,25 @@
-public class Book {
+// Lớp Book
+public class Book extends Item {
+    private String author;
+    private int numberOfPages;
+    private String genre;
+
+    public Book(String name, String description, String ID, float price, String author, int numberOfPages, String genre) {
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Thông tin Sách:");
+        System.out.println("Tên: " + name);
+        System.out.println("Mô tả: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Giá: " + price);
+        System.out.println("Tác giả: " + author);
+        System.out.println("Số trang: " + numberOfPages);
+        System.out.println("Thể loại: " + genre);
+    }
 }
