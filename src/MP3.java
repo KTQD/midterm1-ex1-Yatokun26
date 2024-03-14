27b75e5a49d7a9f8f58b65eb7f1e41b1118fb975
Yato <117742512+Yatokun26@users.noreply.github.com>
2024-03-14 07:50:28 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,18 @@
-public class MP3 {
+public class MP3 extends Item {
+    private int duration;
+
+    public MP3(String name, String description, String ID, float price, int duration) {
+        super(name, description, ID, price);
+        this.duration = duration;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Thông tin MP3:");
+        System.out.println("Tên: " + name);
+        System.out.println("Mô tả: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Giá: " + price);
+        System.out.println("Thời lượng: " + duration);
+    }
 }
