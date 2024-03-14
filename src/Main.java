67bd2948d59f2fb988572a15b8381048b5947c6c
Yato <117742512+Yatokun26@users.noreply.github.com>
2024-03-14 07:50:05 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
+// Lớp Main
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("Máy nghe nhạc MP3", "Máy nghe nhạc cầm tay", "MP3001", 49.99f, 3600);
+        Book book = new Book("Lập trình Java", "Hướng dẫn cho người mới bắt đầu về lập trình Java", "BK001", 29.99f, "John Smith", 300, "Lập trình");
 
-        MP3 mp3;
         mp3.showInfo();
-
-        Book book;
+        System.out.println();
         book.showInfo();
-
     }
 }
